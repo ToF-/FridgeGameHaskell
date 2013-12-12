ff77ab545e37cd1293bb310aad351917a604d823
ToF- <cthibaut@octo.com>
2013-12-12 23:26:57 +0100

    simplifying

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,7 +1,7 @@
 module Main
 where
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Happstack.Server (nullConf, simpleHTTP, toMessage, ok)
 
 main :: IO ()
-main = simpleHTTP nullConf $ ok $ toResponse "42"
+main = simpleHTTP nullConf $ ok $ toMessage "42"
 
