5ce0235d77f69c5fcf6b09918776eb1540bccb76
ToF- <cthibaut@octo.com>
2013-12-12 23:06:46 +0100

    refactoring

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 module Main
 where
 import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
-import Text.JSON.Generic
 
 main :: IO ()
 main = simpleHTTP nullConf $ ok $ toResponse "42"
