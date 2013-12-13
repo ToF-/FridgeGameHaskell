f4e4aa68e5644d85b07530ef222d1c5eb217415f
ToF- <cthibaut@octo.com>
2013-12-13 12:26:55 +0100

    added a page that compute some value from param

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,14 +1,20 @@
 module Main
 where
 import Control.Monad (msum)
+import Data.Text.Lazy (unpack)
 import Happstack.Server (nullConf, simpleHTTP, toMessage, ok, lookText, ServerPart, Response, dir, toResponse)
 import qualified Text.Blaze.Html5 as H
 
 server = msum [dir "serveparam" $ serveParam 
-              ,dir "servepage"  $ servePage]
+              ,dir "servepage"  $ servePage
+              ,dir "compute"    $ serveComputation]
 
 serveParam = do param <- lookText "param" 
                 ok $ toResponse param
+
+serveComputation = do param <- lookText "param"
+                      let value = read $ unpack param
+                      ok $ toResponse (show (value * value))
 
 servePage = ok $ toResponse $
    H.html $ do
@@ -16,6 +22,7 @@ servePage = ok $ toResponse $
        H.title (H.toHtml "simple page")
      H.body $ do
        (H.toHtml "simple body")
+
 
 main :: IO ()
 main = simpleHTTP nullConf server
