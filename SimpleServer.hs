73a947058ad5a7a2076af258b097704265d357ed
ToF- <cthibaut@octo.com>
2013-12-13 12:04:46 +0100

    serve a simple page and a page with parameter as well

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,9 +1,21 @@
 module Main
 where
-import Happstack.Server (nullConf, simpleHTTP, toMessage, ok, lookText, ServerPart, Response)
+import Control.Monad (msum)
+import Happstack.Server (nullConf, simpleHTTP, toMessage, ok, lookText, ServerPart, Response, dir, toResponse)
+import qualified Text.Blaze.Html5 as H
 
-server = do param <- lookText "param" 
-            ok $ toMessage param
+server = msum [dir "serveparam" $ serveParam 
+              ,dir "servepage"  $ servePage]
+
+serveParam = do param <- lookText "param" 
+                ok $ toResponse param
+
+servePage = ok $ toResponse $
+   H.html $ do
+     H.head $ do
+       H.title (H.toHtml "simple page")
+     H.body $ do
+       (H.toHtml "simple body")
 
 main :: IO ()
 main = simpleHTTP nullConf server
