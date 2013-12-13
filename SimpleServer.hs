da42b99c9db65fb6cb305d7dfd1b1a37117d6078
ToF- <cthibaut@octo.com>
2013-12-13 11:49:28 +0100

    refactoring

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,8 +1,11 @@
 module Main
 where
-import Happstack.Server (nullConf, simpleHTTP, toMessage, ok, lookText)
+import Happstack.Server (nullConf, simpleHTTP, toMessage, ok, lookText, ServerPart, Response)
+
+server = do param <- lookText "param" 
+            ok $ toMessage param
 
 main :: IO ()
-main = simpleHTTP nullConf $ do param <- lookText "param" 
-                                ok $ toMessage param
+main = simpleHTTP nullConf server
+
 
