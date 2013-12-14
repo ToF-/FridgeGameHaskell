70e1823568073763bc03e53f2d8bd1f156a1180f
ToF- <cthibaut@octo.com>
2013-12-14 22:47:14 +0100

    Refrigerated Room logic complete

--- RefrigeratedRoom.hs
+++ RefrigeratedRoom.hs
@@ -1,10 +1,22 @@
 module RefrigeratedRoom
 where
 
-data RefrigeratedRoom = Room { temperature :: Double, position :: Int }
+data RefrigeratedRoom = Room { temperatures :: [Double], position :: Int }
+    deriving (Show, Eq)
+
+temperature :: RefrigeratedRoom -> Double
+temperature = head . temperatures
+
+initialTemperatures :: [Double]
+initialTemperatures = take 5 (repeat 15)
 
 newRoom :: RefrigeratedRoom 
-newRoom = Room 15.0 100
+newRoom = Room initialTemperatures 100
 
 update :: RefrigeratedRoom -> RefrigeratedRoom
-update (Room t p) = Room (t-1) p
+update r = let 
+        t = temperature r
+        ts = temperatures r
+        p  = fromIntegral (position r)
+        delta = (p / 10.0 + 2.0 - ts !! 4) / 3.0
+    in r { temperatures = (t+delta):ts }
--- Specs.hs
+++ Specs.hs
@@ -1,6 +1,9 @@
 import Test.Hspec
 import RefrigeratedRoom
 
+apply :: Int -> (a -> a) -> a -> a
+apply n f i = (iterate f i) !! n
+
 main :: IO ()
 main = hspec $ do
     describe "A refrigerated room\n" $ do
@@ -12,9 +15,13 @@ main = hspec $ do
 
         it "should have its temperature evolve after update" $ do
             temperature (update newRoom) `shouldBe` 14.0
-            temperature (update (update newRoom)) `shouldBe` 13.0
+            temperature (apply 2 update newRoom) `shouldBe` 13.0
 
         it "should allow its position to be modified" $ do
             position (newRoom { position = 50 }) `shouldBe` 50
 
-           
+        it "should have its temperature decreasing differently after 5 updates" $ do
+            temperature (apply 6 update newRoom) `shouldBe` 9.333333333333334   
+
+        it "should have its temperature decreasing differently according to position" $ do
+            temperature (update (newRoom { position = 50 })) `shouldBe` 12.333333333333334
