70465558238c55cf4d3183f717634e997afddb7c
ToF- <cthibaut@octo.com>
2013-12-15 19:54:04 +0100

    console simulation runner complete

--- TextSimulationRunner.hs
+++ TextSimulationRunner.hs
@@ -1,13 +1,86 @@
 module Main
 where
+import Control.Concurrent
+import Simulation
+import Report
 
 printInstructions :: IO ()
 printInstructions = putStrLn $ unlines
      ["REFRIGERATED ROOM SIMULATION"
-     ,"START <n> : start the simulator for n turns"
+     ,"START     : start the simulator"
      ,"POS   <n> : set the position [0..100] for the command"
      ,"<empty>   : let the simulator unchanged"
      ,"STOP      : stop the simulator and quit"
-     ,"REINIT    : put the simulator back to initial state"]
+     ,"REINIT    : put the simulator back to initial state"
+     ,"HELP      : print these instructions"]
+
+printSimulation :: MVar Simulation -> IO ()
+printSimulation v = do s <- readMVar v
+                       putStrLn $ showSimulation s
+
+updateSimulation :: MVar Simulation -> IO ()
+updateSimulation v = 
+    do s <- takeMVar v
+       let s' = case updateRoom s of
+            Right u -> u
+            Left  _ -> s
+       putMVar v s'
+
+startSimulation :: MVar Simulation -> IO ()
+startSimulation v = 
+    do s <- takeMVar v
+       let s' = start s
+       putMVar v s'
+
+reinitSimulation :: MVar Simulation -> IO ()
+reinitSimulation v =
+    do s <- takeMVar v
+       let s' = newSimulation 
+       putMVar v s'
+
+isInteger :: String -> Bool
+isInteger s = case reads s :: [(Integer, String)] of
+  [(_, "")] -> True
+  _         -> False
+
+setPositionSimulation :: String ->MVar Simulation -> IO ()
+setPositionSimulation p v = 
+    case isInteger p of
+        False -> do putStrLn ("NOT AN INTEGER:" ++ p)
+                    return ()
+        True  -> do s <- takeMVar v
+                    s' <- case setPosition s (read p) of
+                                Left msg -> do putStrLn msg
+                                               return s
+                                Right u  -> return u
+                    putMVar v s'
+      
+
+readEvalPrintLoop :: MVar Simulation -> IO ()
+readEvalPrintLoop v = 
+    do printSimulation v
+       e <- getLine
+       let (continue, action) = case (words e) of
+            ["HELP"]  -> (True,  printInstructions)
+            ["STOP"]  -> (False, putStrLn "STOPPING THE SIMULATION")
+            ["START"] -> (True,  startSimulation v)
+            ["REINIT"]-> (True,  reinitSimulation v)
+            ["POS",n] -> (True,  setPositionSimulation n v)
+            []        -> (True,  return ())
+            _       -> (True,  putStrLn ("UNKNWON COMMAND:" ++ e))
+       action
+       updateSimulation v
+       case continue of
+        True  -> readEvalPrintLoop v
+        False -> return ()
+
+printReport :: MVar Simulation -> IO ()
+printReport v =
+    do s <- readMVar v 
+       putStrLn (pretty (report s))
 
 main = do printInstructions
+          v <- newMVar newSimulation
+          readEvalPrintLoop v
+          printReport v
+         
