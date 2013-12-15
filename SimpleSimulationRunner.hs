f8a3e9e3d782e5dc236b602c3ad593c411719f79
ToF- <cthibaut@octo.com>
2013-12-15 12:22:24 +0100

    a simple non interactive simulation runner

--- SimpleSimulationRunner.hs
+++ SimpleSimulationRunner.hs
@@ -1,9 +1,18 @@
 module Main
 where
+import RefrigeratedRoom
 import Simulation
 import Report
 
+process :: Int -> Simulation -> IO ()
+process 0 s = do putStrLn "SIMULATION COMPLETE"
+                 putStrLn (show (report s))
+
+process n s = do putStrLn $ (show (position (room s))) ++ " " ++ (show (temperature (room s)))
+                 case updateRoom s of
+                    Left m -> do putStrLn m
+                                 process n s
+                    Right s' -> process (n-1) s'
+
 main = do putStrLn "SIMULATION.. CTL-C TO QUIT"
-          let sim = newSimulation
-              sts = recordState sim []
-          putStrLn (show (report sts))
+          process 20 $ start newSimulation
