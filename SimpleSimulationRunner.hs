de165cd84457398360cb24ebd53de4582700eaf3
ToF- <cthibaut@octo.com>
2013-12-15 16:43:39 +0100

    interactive runner (flawed one)

--- SimpleSimulationRunner.hs
+++ SimpleSimulationRunner.hs
@@ -4,15 +4,24 @@ import RefrigeratedRoom
 import Simulation
 import Report
 
-process :: Int -> Simulation -> IO ()
-process 0 s = do putStrLn "SIMULATION COMPLETE"
-                 putStrLn (pretty $ report s)
+process :: Int -> Int -> Simulation -> IO ()
+process _ 0 s = do putStrLn "SIMULATION COMPLETE"
+                   putStrLn (pretty $ report s)
 
-process n s = do putStrLn $ (show (position (room s))) ++ " " ++ (show (temperature (room s)))
-                 case updateRoom s of
-                    Left m -> do putStrLn m
-                                 process n s
-                    Right s' -> process (n-1) s'
+process max n s = do putStrLn $ (show (position (room s))) ++ " " ++ (show (temperature (room s)))
+                     putStrLn "COMMAND: (0..100 / STOP / REINIT)"
+                     entry <- getLine
+                     let (n',s') = case entry of 
+                                    "START" -> (max, start newSimulation)
+                                    "STOP" -> (0, stop s)
+                                    "REINIT" -> (max, newSimulation)
+                                    n -> ((read n)-1, case setPosition s (read n) of
+                                                        Left m -> s
+                                                        Right x-> x)
+                     case updateRoom s' of
+                         Left m -> do putStrLn m
+                                      process max n' s'
+                         Right s' -> process max (n'-1) s'
 
 main = do putStrLn "SIMULATION.. CTL-C TO QUIT"
-          process 20 $ start newSimulation
+          process 20 20 $ start newSimulation
